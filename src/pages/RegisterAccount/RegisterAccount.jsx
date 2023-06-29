ba791d9d67ee76a9338e8ba9eb1d455ccdceff46
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-06-29 15:23:10 -0300

    Refactoring user creation form

--- src/pages/RegisterAccount/RegisterAccount.jsx
+++ src/pages/RegisterAccount/RegisterAccount.jsx
@@ -1,5 +1,32 @@
-const RegisterAccount = () => {
+import { useState } from "react";
+import InputMask from 'react-input-mask';
 
+const RegisterAccount = () => {
+  const [repeatPassword , setRepeatPassword] = useState('');
+  const [user, setUser] = useState({
+    email: '',
+    telephone: ''
+  })
+
+  const handleClickRegister = (e) => {
+    e.preventDefault();
+    if(checkPassword && user.email){
+      console.log('ok')
+      return
+    }
+    console.log('deu errado')
+  }
+
+  const handleClickUser = (e , prop) => {
+    const object = { ...user };
+    object[prop] = e;
+    setUser(object)
+  }
+
+  const checkPassword = () => {
+    return (user.password.length > 0) && 
+           (user.password === repeatPassword) ? true : false;
+  }
 
   return (
      <section class="bg-gray-50 dark:bg-gray-900">
@@ -15,24 +42,35 @@ const RegisterAccount = () => {
             </h1>
             <form className="space-y-4 md:space-y-6">
           <div class="mb-6">
-            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Seu email</label>
-            <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required/>
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome</label>
+            <input type="email" id="email" name="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" onChange={e => handleClickUser(e.target.value , 'name')} required/>
           </div>
           <div class="mb-6">
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+            <input type="email" id="email" name="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@email.com" onChange={e => handleClickUser(e.target.value , 'email')} required/>
+          </div>
+
+          <div class="mb-6">
+            <label htmlFor="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Telefone</label>
+            <InputMask type="tel" id="phone" mask="(99) 99999-9999" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" onChange={e => handleClickUser(e.target.value, 'telephone')} required/>
+          </div>
+
+
+          {/* <div class="mb-6">
             <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sua senha</label>
-            <input type="password" id="password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required/>
+            <input type="password" id="password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" onChange={e => handleValuePassword(e.target.value)}/>
           </div>
           <div class="mb-6">
             <label for="repeat-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Repita a senha</label>
-            <input type="password" id="repeat-password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required/>
-          </div>
+            <input type="password" id="repeat-password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" onChange={e => handleClickRepeatPassword(e.target.value)}/>
+          </div> */}
           <div class="flex items-start mb-6">
             <div class="flex items-center h-5">
-              <input id="terms" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required/>
+              <input id="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" />
             </div>
             <label for="terms" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Eu aceito os <a href="#" class="text-blue-600 hover:underline dark:text-blue-500">termos e condições</a></label>
           </div>
-          <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Registrar conta</button>
+          <button class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={handleClickRegister}>Registrar conta</button>
         </form>
           </div>
         </div>
